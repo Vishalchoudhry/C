4ee67e9c47d290e571b329568f209793de3e31c2
Vishal Choudhary <73791462+Vishalchoudhry@users.noreply.github.com>
2021-04-21 10:13:32 +0530

    Delete C directory

--- C/c02.c
+++
@@ -1,9 +0,0 @@
-//Copyright© 2021 Vishal Choudhary.
-//Program
-#include<stdio.h>
-int main()
-{
-    printf("Hello World!\nbut Really!");
-    return 0;
-
-};
--- C/c04.c
+++
@@ -1,8 +0,0 @@
-//Copyright© 2021 Vishal Choudhary.
-#include<stdio.h>
-int main()
-{
-    printf("\n\"What Do you Know About Rollin Down in The Deep\"\n|By:| \"Vishal Choudhary\"");
-    return 0;
-
-};
